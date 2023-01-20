0e5c855da76a5a209120410227b99a3bc0d7b87f
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-01-20 14:02:29 +0800

    working add to cart features

--- script.js
+++ script.js
@@ -1,159 +1,139 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-dark')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+function purchaseClicked() {
+    alert('Thank you for your purchase')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+    
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('This item is already added to the cart')
+            return
+        }
+       
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-dark" type="button">REMOVE</button>
+        </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-dark')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+    
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('₱', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '₱' + total
+}
 
 
+// Modal Function
 
 
 
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var icon = document.getElementById("icon");
+var span = document.getElementById("addtocartclose");
 
 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
+icon.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function imgloop()
-// {
-//   var ar = [];
-//   ar.push("./img/cards/ps4.png");
-//   ar.push("./img/cards/ps2.png");
-//   ar.push("./img/cards/ps1.png");
-
-//   for (i = 0; ar.length > i; i++)
-//   {
-//     var img = new Image(200, 200);
-//     img.src = ar[i];
-//     var src = document.getElementById("elementid");
-//     // var src = document.querySelectorAll('.image-box');
-//     src.appendChild(img);
-//   }
-
-// }
-  
-// function imgloop()
-// {
-//   var ar = [];
-//   ar.push("./img/cards/ps4.png");
-//   ar.push("./img/cards/ps2.png");
-//   ar.push("./img/cards/ps1.png");
-
-//   for (i = 0; ar.length > i; i++)
-//   {
-//     var img = new Image(200, 200);
-//     img.src = ar[i];
-//     var src = document.getElementById("elementid");
-//     // var src = document.querySelectorAll('.image-box');
-//     src.appendChild(img);
-//   }
-
-// }
-
-
-// const boxes = document.querySelectorAll('.image-box');
-
-
-// for (let i = 0; i < boxes.length; i++) {
-//   // boxes[i].style.backgroundColor = "blue";
-// }
-
-
-
-
-// //  
-
-
-// let div = document.createElement("div")
-// let p = document.createElement("p")
-// div.append("Some text", p)
-
-
-
-
-// function myFunction() {
-//   const boxes = document.querySelectorAll('image-box');
-
-//   for (let i = 0; i < boxes.length; i++) {
-//   var x = document.createElement("IMG");
-//   x.setAttribute("src", images[0]);
-//   x.setAttribute("alt", "ps5");
-//   boxes.appendChild(x);
-//   }
-//  }
-
-
-// // function myFunction() {
-
-// //   var x = document.createElement("IMG");
-// //    x.setAttribute("src", images[0]);
-// //    x.setAttribute("alt", "ps5");
-// //    document.getElementById('box').appendChild(x);
-// // }
-
-
-
-// myFunction()
-
-
-
-
-
-
-// // function createImage () {
-// //   var img = document.createElement('img');
-// //   img.src = images[0]
-// //   document.getElementById('box-one').appendChild(img);
-// //   down.innerHTML = "Image Element Added.";
-// // }
-// // createImage()
-
-
-// // // let imageHolder = document.getElementsByClassName(".image-box").document.body.appendChild(x);
-
-
-// // function myFunction() {
-// //   var x = document.createElement("IMG");
-// //   // let imageHolder = document.getElementsById("ronan")
-// //   x.setAttribute("src", images[0]);
-// //   // x.setAttribute("width", "304");
-// //   // x.setAttribute("height", "228");
-// //    x.setAttribute("alt", "ps5");
-// //   //  document.body.appendChild(x);
-// //    document.getElementById('ronan').appendChild(x);
-// // }
